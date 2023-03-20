f0c73e6d6cb7fee98a8442e8e4529210e2cab88f
josy Santos <josydossantos2002@gmail.com>
2023-03-20 13:51:49 +0000

    more one section

--- assets/js/basics.js
+++ assets/js/basics.js
@@ -32,3 +32,11 @@ if(CreateAccountBtn.length != 0)
     CreateAccountBtn[0].onclick = () => {
     }
 }
+
+const btnToShowTheContact = document.querySelectorAll("#clik-to-contact")
+const inputCheckToDivInfo = document.querySelectorAll("#t-check-div-info")
+if((btnToShowTheContact.length && inputCheckToDivInfo).length != 0){
+    btnToShowTheContact[0].onclick = () => {
+        inputCheckToDivInfo[0].checked = true
+    }
+}
